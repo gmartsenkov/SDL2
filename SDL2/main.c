5446fd8f798f0ce66d3fed28e8399c3f29a29bf5
Georgi Martsenkov <g.martsenkov@gmail.com>
2016-12-10 18:08:29 +0000

    Initial Commit

---
+++ SDL2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SDL2
+//
+//  Created by Gogo on 12/10/16.
+//  Copyright © 2016 Gogo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
