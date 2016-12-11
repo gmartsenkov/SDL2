89a121062c46f142fa83c382e23cfd47169c4653
Georgi Martsenkov <g.martsenkov@gmail.com>
2016-12-11 23:12:14 +0000

    init

--- SDL2/main.c
+++ SDL2/main.c
@@ -1,15 +1,37 @@
-//
-//  main.c
-//  SDL2
-//
-//  Created by Gogo on 12/10/16.
-//  Copyright © 2016 Gogo. All rights reserved.
-//
+// Example program:
+// Using SDL2 to create an application window
 
+#include <SDL2/SDL.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(int argc, char* argv[]) {
+    SDL_Window *window;                    // Declare a pointer
+    SDL_Surface* screenSurface = NULL;
+    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+                              "An SDL2 window",                  // window title
+                              SDL_WINDOWPOS_UNDEFINED,           // initial x position
+                              SDL_WINDOWPOS_UNDEFINED,           // initial y position
+                              640,                               // width, in pixels
+                              480,                               // height, in pixels
+                              SDL_WINDOW_OPENGL                  // flags - see below
+                              );
+    // Check that the window was successfully made
+    if (window == NULL) {
+        // In the event that the window could not be made...
+        printf("Could not create window: %s\n", SDL_GetError());
+        return 1;
+    } else {
+        screenSurface = SDL_GetWindowSurface( window );
+        SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ));
+        SDL_UpdateWindowSurface(window);
+    }
+    // The window is open: enter program loop (see SDL_PollEvent)
+    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+    // Close and destroy the window
+    SDL_DestroyWindow(window);
+    // Clean up
+    SDL_Quit();
     return 0;
 }
